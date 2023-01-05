b36e0f3541c0a60e4518d656f74cb43ae7ab7aae
lucksmith-wd <116928478+lucksmith-wd@users.noreply.github.com>
2023-01-05 22:15:14 +0800

    a

--- script.js
+++ script.js
@@ -42,9 +42,7 @@ function validateOnSubmit(inputs) {
 const inputFields = [...document.querySelectorAll('.data-entry .form-item:nth-child(-n+3)>input')]
 inputFields.forEach((field) => field.addEventListener('blur', validate));
 
-
 // *********************Business logic****************************
-
 function Book(allInputs) {
   for (let i = 0; i < allInputs.length - 1; i++) {
     let input = allInputs[i];
@@ -54,11 +52,13 @@ function Book(allInputs) {
 }
 
 function removeBook(e) {
-  const cardToRemove = e.target.parentNode;
-  console.log(cardToRemove.dataset.index);
+  const cardToRemove = e.target.localName === "img"
+    ? e.target.parentNode.parentNode
+    : e.target.parentNode;
+  console.dir(e.target);
   myLibrary.splice(cardToRemove.dataset.index, 1);
   displayAllCards();
-
+  displayAllNodes();
 }
 
 function addBookToLibrary(e) {
@@ -66,47 +66,51 @@ function addBookToLibrary(e) {
   if (!validateOnSubmit([...e.target].splice(0, 3))) {
     return;
   }
-
   const allInputs = [...inputFields, document.querySelector('.data-entry #finished')];
-  const book = new Book(allInputs);
-
+  const book = new Book([...allInputs]);
   myLibrary.push(book);
-
-  displayAllCards()
-
-  console.table(myLibrary);
+  displayAllCards();
+  displayAllNodes();
   inputFields.forEach((field) => field.value = "");
+  console.log(allInputs[allInputs.length - 1]);
   allInputs[allInputs.length - 1].checked = false;
 }
 
-
+// To refactor -- repetition
 function displayAllCards() {
-  const cardContainerNode = document.querySelector('.card-container');
-  removeAllCards(cardContainerNode);
-  myLibrary.forEach((book) => cardContainerNode.appendChild(createCard(book)));
+  const containerNode = document.querySelector('.card-container');
+  removeAllFromDom(containerNode);
+  myLibrary.forEach((book) => containerNode.appendChild(createCard(book)));
 }
 
-function removeAllCards(cardContainerNode) {
-  while (cardContainerNode.firstChild) {
-    cardContainerNode.removeChild(cardContainerNode.firstChild);
+function displayAllNodes() {
+  const containerNode = document.querySelector('.book-list');
+  removeAllFromDom(containerNode);
+  myLibrary.forEach((book) => containerNode.appendChild(createNotice(book)));
+}
+// -----------------------------
+
+function removeAllFromDom(containerNode) {
+  while (containerNode.firstChild) {
+    containerNode.removeChild(containerNode.firstChild);
   }
 }
 
 function toggleRead(e) {
   const i = e.target.parentNode.parentNode.dataset.index;
   myLibrary[i].isRead = !myLibrary[i].isRead;
-
-  e.target.parentNode.classList.toggle('read');
-  e.target.parentNode.classList.toggle('unread');
-
-  e.target.src = `images/icon-${myLibrary[i].isRead ? 'read-brand' : 'unread'}.svg`;
-
-
+  const cardIconWrapper = document.querySelector(`.card[data-index="${i}"] .icon-wrapper`);
+  const noticeIconWrapper = document.querySelector(`.book[data-index="${i}"] .icon-wrapper`);
+  cardIconWrapper.classList.toggle('read');
+  cardIconWrapper.classList.toggle('unread');
+  noticeIconWrapper.classList.toggle('read');
+  noticeIconWrapper.classList.toggle('unread');
+  cardIconWrapper.firstChild.src = `images/icon-${myLibrary[i].isRead ? 'read-brand' : 'unread'}.svg`;
+  noticeIconWrapper.firstChild.src = `images/icon-${myLibrary[i].isRead ? 'read' : 'unread'}.svg`;
 }
 
 /*********************Creating Cards************************************/
-
-function createIconWrapperNode(book) {
+function createIconWrapperNode(book, readIconFile, unreadIconFile) {
   const iconWrapperNode = document.createElement('div');
   const status = book.isRead ? 'read' : 'unread';
   iconWrapperNode.classList.add('icon-wrapper');
@@ -114,7 +118,7 @@ function createIconWrapperNode(book) {
   const icon = document.createElement('img');
   icon.classList.add('btn');
   icon.classList.add(`icon-${status}`);
-  icon.src = `images/icon-${status === 'read' ? 'read-brand' : 'unread'}.svg`;
+  icon.src = `${book.isRead ? readIconFile : unreadIconFile}`;
   icon.alt = `Icon for ${status} book`;
   iconWrapperNode.appendChild(icon);
   iconWrapperNode.addEventListener('click', toggleRead);
@@ -163,15 +167,54 @@ function createCard(book) {
   const cardNode = document.createElement('div');
   cardNode.classList.add('card');
   cardNode.setAttribute('data-index', myLibrary.indexOf(book));
-  cardNode.append(createIconWrapperNode(book));
-  cardNode.append(createTitleNode(book));
-  cardNode.append(createAuthorNode(book));
-  cardNode.append(createOtherInfoNode(book));
+  cardNode.appendChild(createIconWrapperNode(book, 'images/icon-read-brand.svg', 'images/icon-unread.svg'));
+  cardNode.appendChild(createTitleNode(book));
+  cardNode.appendChild(createAuthorNode(book));
+  cardNode.appendChild(createOtherInfoNode(book));
   cardNode.appendChild(createRemoveBtnNode(book));
   return cardNode;
 }
 
 /***********************************************************************/
+
+function createOtherNoticeInfo(book) {
+  const noticeInfoNode = document.createElement('div');
+  noticeInfoNode.classList.add('other-info');
+  const author = document.createElement('span');
+  author.classList.add('author');
+  author.textContent = book.author;
+  noticeInfoNode.append(author);
+  const pages = document.createElement('span');
+  pages.classList.add('pages');
+  pages.textContent = ` (${book.numPages} pages)`;
+  noticeInfoNode.append(pages);
+  return noticeInfoNode;
+}
+
+function createIconWrapperForDelete(book) {
+  const iconWrapperNode = document.createElement('div');
+  iconWrapperNode.classList.add('icon-wrapper');
+  iconWrapperNode.classList.add('delete');
+  const icon = document.createElement('img');
+  icon.classList.add('btn');
+  icon.classList.add(`icon-delete`);
+  icon.src = 'images/icon-remove.svg';
+  icon.alt = `Icon for delete book icon`;
+  iconWrapperNode.appendChild(icon);
+  iconWrapperNode.addEventListener('click', removeBook);
+  return iconWrapperNode;
+}
+
+function createNotice(book) {
+  const noticeNode = document.createElement('li');
+  noticeNode.classList.add('book');
+  noticeNode.setAttribute('data-index', myLibrary.indexOf(book));
+  noticeNode.appendChild(createTitleNode(book));
+  noticeNode.appendChild(createOtherNoticeInfo(book));
+  noticeNode.appendChild(createIconWrapperNode(book, 'images/icon-read.svg', 'images/icon-unread.svg'));
+  noticeNode.appendChild(createIconWrapperForDelete(book));
+  return noticeNode;
+}
 
 const form = document.querySelector('.data-entry');
 form.addEventListener('submit', addBookToLibrary);
